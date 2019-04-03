3bfd4f1d9d9dc7c18315782eb13154f154520860
Tommy Škrak <39591025+karikiram@users.noreply.github.com>
2019-04-03 11:50:59 +0200

    Add files via upload

---
+++ PreglednikDatoteka-GB-FULL-PATH/DatotecniSustav01/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace DatotecniSustav01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string direktorij = Console.ReadLine();
+            DirectoryInfo dirInfo = new DirectoryInfo(direktorij);
+
+            var datoteke = dirInfo.GetFiles();
+            decimal velicina = 0;
+            string nazivi = "Nazivi datoteka:";
+            //string extension = Path.GetExtension(direktorij);
+            //string filename = Path.GetFileName(direktorij);
+            //string root = Path.GetPathRoot(direktorij);
+
+            // detaljan ispis veličine fajlova u datoteci
+
+            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+            Console.WriteLine("| Veličina       B |          KB |              MB |              GB |                TB | {0, 20} |",nazivi);
+            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+            foreach (FileInfo d in datoteke)
+            {
+                string directoryFullPath = Path.GetFullPath(d.FullName);
+                velicina += d.Length;
+                Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 8} MB     | {3, 8} GB     | {4, 8} TB       | {5,20} |",
+                    d.Length,
+                    d.Length / 1024,
+                    d.Length / (1024 * 1024),
+                    Math.Round((decimal)d.Length / (1024 * 1024 * 1024), 2),
+                    Math.Round((decimal)d.Length / (1024 * 1024 * 1024) / 1024, 6),
+                    directoryFullPath);
+
+            }
+            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+            Console.WriteLine("|{0, 15} B | {1, 8} KB | {2, 8} MB     | {3, 8} GB     | {4, 8} TB       |                      |",
+                Math.Round(velicina, 0),
+                Math.Round(velicina / (1024), 0),
+                Math.Round(velicina / (1024 * 1024), 0),
+                Math.Round(velicina / (1024 * 1024 * 1024), 2),
+                Math.Round(velicina / (1024 * 1024 * 1024) / 1024, 6)); 
+            Console.WriteLine("+------------------+-------------+---------+------------------------------------------+");
+
+            Console.SetCursorPosition(1, 3);
+            Console.Write(">");
+            int brojRedova = datoteke.Length + 6;
+
+            int cekanjeTreperenje = 500;
+            Console.CursorVisible = false;
+            int pokazivacY = 3;
+            while (true)
+            {
+                System.Threading.Thread.Sleep(cekanjeTreperenje);
+                Console.SetCursorPosition(1, pokazivacY);
+                Console.Write(" ");
+                System.Threading.Thread.Sleep(cekanjeTreperenje);
+                Console.SetCursorPosition(1, pokazivacY);
+                Console.Write(">");
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pritisnutaTipka = Console.ReadKey(true);
+                    if (pritisnutaTipka.Key == ConsoleKey.DownArrow)
+                    {
+                        Console.SetCursorPosition(1, pokazivacY);
+                        Console.Write(" ");
+                        pokazivacY++;
+                    }
+                }
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pritisnutaTipka3 = Console.ReadKey(true);
+                    if (pritisnutaTipka3.Key == ConsoleKey.UpArrow)
+                    {
+                        Console.SetCursorPosition(1, pokazivacY);
+                        Console.Write(" ");
+                        pokazivacY--;
+                    }
+                }
+
+                // metoda za otvaranje datoteke u kojoj se nalaze fajlovi
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo pritisnutaTipka2 = Console.ReadKey(true);
+                    if (pritisnutaTipka2.Key == ConsoleKey.D)
+                    {
+                        //if (!File.Exists(direktorij))
+                        //{
+                        //    return;
+                        //}
+                        Process.Start("explorer.exe", direktorij);
+                    }
+
+                }
+
+            }
+
+            // Console.SetCursorPosition(0, brojRedova);
+        } //Main
+    }
+}
